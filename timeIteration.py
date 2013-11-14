36d09c7d44a72c98c20fd002cf492314ed367d99
Toby Searle <T.W.Searle@sms.ed.ac.uk>
2013-11-14 00:14:09 +0000

    Added more terms and tests to time iteration program. Changed out put file name in one program.

--- timeIteration.py
+++ timeIteration.py
@@ -3,7 +3,7 @@
 #   SSP
 #   timeIteration.py
 # 
-#   Last modified: Mon 11 Nov 17:44:01 2013
+#   Last modified: Wed 13 Nov 22:10:56 2013
 #--------------------------------------------------------------------------------------------------
 
 """ Time Iteration Program for the Linear Stability Analysis of the Viscoelastic SSP
@@ -25,7 +25,7 @@ from scipy.sparse import linalg as sparselinalg
 import cPickle as pickle
 import ConfigParser
 
-# PARAMETERS
+# PARAMETERS -----------------------------------------------------------------
 startTime = time.time()
 
 config = ConfigParser.RawConfigParser()
@@ -41,9 +41,13 @@ kx = config.getfloat('settings', 'k')
 
 fp.close()
 
+TIMESTEP = 0.0001
+numTimeSteps = 10
+
 filename = '-N{N}-M{M}-Re{Re}-b{beta}-Wi{Wi}-amp{Amp}.pickle'.format(\
             N=N,M=M,Re=Re,beta=beta,Wi=Wi,Amp=Amp)
 
+# -----------------------------------------------------------------------------
 # FUNCTIONS
 
 def mk_single_diffy():
@@ -150,6 +154,7 @@ psi = zeros(((2*N+1)*M), dtype='complex')
 # Read in the base profile
 (U,V,W,Cxx,Cyy,Czz,Cxy,Cxz,Cyz) = pickle.load(open('pf'+filename, 'r'))
 
+
 #### MAKE OPERATORS ####
 
 # basic operators
@@ -163,10 +168,45 @@ MDYZZ = dot(MDY,MDZZ)
 MDYYZ = dot(MDYY,MDZ)
 MDZZZ = dot(MDZ,MDZZ)
 
-LAPLACIAN = -(kx**2)*eye((2*N+1)*M, (2*N+1)*M) + MDYY + MDZZ
 
 singleMDY = mk_single_diffy()
 singleMDYY = dot(singleMDY, singleMDY)
+
+# vectors
+MMU = prod_mat(U)
+MMV = prod_mat(V)
+MMW = prod_mat(W)
+MMCXX = prod_mat(Cxx)
+MMCYY = prod_mat(Cyy)
+MMCZZ = prod_mat(Czz)
+MMCXY = prod_mat(Cxy)
+MMCXZ = prod_mat(Cxz)
+MMCYZ = prod_mat(Cyz)
+ 
+MMDYU = prod_mat(dot(MDY,U))
+MMDYV = prod_mat(dot(MDY,V))
+MMDYW = prod_mat(dot(MDY,W))
+MMDYCXX = prod_mat(dot(MDY,Cxx))
+MMDYCYY = prod_mat(dot(MDY,Cyy))
+MMDYCZZ = prod_mat(dot(MDY,Czz))
+MMDYCXY = prod_mat(dot(MDY,Cxy))
+MMDYCXZ = prod_mat(dot(MDY,Cxz))
+MMDYCYZ = prod_mat(dot(MDY,Cyz))
+
+MMDZU = prod_mat(dot(MDZ,U))
+MMDZV = prod_mat(dot(MDZ,V))
+MMDZW = prod_mat(dot(MDZ,W))
+MMDZCXX = prod_mat(dot(MDZ,Cxx))
+MMDZCYY = prod_mat(dot(MDZ,Cyy))
+MMDZCZZ = prod_mat(dot(MDZ,Czz))
+MMDZCXY = prod_mat(dot(MDZ,Cxy))
+MMDZCXZ = prod_mat(dot(MDZ,Cxz))
+MMDZCYZ = prod_mat(dot(MDZ,Cyz))
+
+# Complex operators
+
+LAPLACIAN = -(kx**2)*II + MDYY + MDZZ
+VGRAD = 1.j*kx*II*U + dot(MMV,MDY) + dot(MMW,MDZ) 
 
 # Boundary arrays
 BTOP = ones(M)
@@ -215,7 +255,8 @@ del i, n
 
 # The zeroth mode operators
 dw0thOp = -kx**2*eye(M,M) + singleMDYY
-dv0thOp = (-kx**2*eye(M,M) + singleMDYY)*(eye(M,M) + (1./kx**2)*singleMDYY)
+dv0thOp = dot((-kx**2*eye(M,M) + singleMDYY), (eye(M,M) +
+                                               (1./kx**2)*singleMDYY))
 
 # The zeroth mode boundary conditions
 dw0thOp[M-2,:] = BTOP
@@ -226,98 +267,197 @@ dv0thOp[M-1,:] = BBOT
 
 # use solve or invert?
 
+## SET UP A GUESS FOR THE INITIAL CONFORMATION PROFILE
+cvec = zeros((6*vecLen),dtype='complex')
+cvec = (random.random(6*vecLen) + 1.j*random.random(6*vecLen))/1000000.0
 
+outFile = open("strength{0}".format(filename), 'w')
 
 #### ITERATE THE INSTABILITY ####
+for currTime in range(numTimeSteps):
 
-# As a test, use only the stresses to recreate a disturbance.
-# need to make a reasonable disturbance using Oldroyd-B2-full.py
+    dcxx = cvec[:1*vecLen]
+    dcyy = cvec[vecLen:2*vecLen]
+    dczz = cvec[2*vecLen:3*vecLen]
+    dcxy = cvec[3*vecLen:4*vecLen]
+    dcxz = cvec[4*vecLen:5*vecLen]
+    dcyz = cvec[5*vecLen:6*vecLen]
 
-(du_test,dv_test,dw_test,dp_test,dcxx,dcyy,dczz,dcxy,dcxz,dcyz) = pickle.load(
-                                    open('full-evecs-k0.7'+filename,'r'))
+    # As a test, use only the stresses to recreate a disturbance.
+    # need to make a reasonable disturbance using Oldroyd-B2-full.py
 
-### MAKE STREAM FUNCTIONS
+    # (du_test,dv_test,dw_test,dp_test,dcxx,dcyy,dczz,dcxy,dcxz,dcyz) = pickle.load(
+    #                                     open('lead-evec-k0.7'+filename,'r'))
+    # print "magnitude of test disturbance ", linalg.norm(du_test,2)
+    # print "magnitude of base profile ", linalg.norm(U, 2)
 
-# Make the vectors from stresses to calculate phi and psi
-# Stress disturbances are equal to confirmation disturbances in Oldroyd-B
+    ### MAKE STREAM FUNCTIONS
 
-phiStressVec = - kx**2*dot(MDY,dcxx) \
-               + dot( (kx**2*MDY  - MDYZZ), dcyy) + dot(MDYZZ,dczz) \
-               + dot( ((1.j*kx**3)*II - 1.j*kx*MDZZ \
-                      + 1.j*kx*MDYY), dcxy) \
-               + dot( 2.j*kx*MDYZ, dcxz) \
-               + dot( (kx**2*MDZ + MDYYZ - MDZZZ), dcyz) 
-phiStressVec = -((1.-beta)/(beta*Wi))*phiStressVec
+    # Make the vectors from stresses to calculate phi and psi
+    # Stress disturbances are equal to confirmation disturbances in Oldroyd-B
 
-psiStressVec = dot(1.j*kx*MDZ, dcxx) + dot(MDYZ,dcxy) \
-               + dot((MDZZ + kx**2*II), dcxz) \
-               - dot(1.j*kx*MDY, dcyz) - dot(1.j*kx*MDZ, dczz)
-psiStressVec = ((1-beta)/(beta*Wi))*psiStressVec
+    phiStressVec = - kx**2*dot(MDY,dcxx) \
+                   + dot( (kx**2*MDY  - MDYZZ), dcyy) + dot(MDYZZ,dczz) \
+                   + dot( ((1.j*kx**3)*II - 1.j*kx*MDZZ \
+                          + 1.j*kx*MDYY), dcxy) \
+                   + dot( 2.j*kx*MDYZ, dcxz) \
+                   + dot( (kx**2*MDZ + MDYYZ - MDZZZ), dcyz) 
+    phiStressVec = -((1.-beta)/(beta*Wi))*phiStressVec
 
-# insert boundary conditions
+    psiStressVec = dot(1.j*kx*MDZ, dcxx) + dot(MDYZ,dcxy) \
+                   + dot((MDZZ + kx**2*II), dcxz) \
+                   - dot(1.j*kx*MDY, dcyz) - dot(1.j*kx*MDZ, dczz)
+    psiStressVec = ((1-beta)/(beta*Wi))*psiStressVec
 
-phiStressVec[M-4:(2*N+1)*M:M] = 0
-phiStressVec[M-3:(2*N+1)*M:M] = 0
-phiStressVec[M-2:(2*N+1)*M:M] = 0
-phiStressVec[M-1:(2*N+1)*M:M] = 0
+    # insert boundary conditions
 
-psiStressVec[M-2:(2*N+1)*M:M] = 0
-psiStressVec[M-1:(2*N+1)*M:M] = 0
+    phiStressVec[M-4:(2*N+1)*M:M] = 0
+    phiStressVec[M-3:(2*N+1)*M:M] = 0
+    phiStressVec[M-2:(2*N+1)*M:M] = 0
+    phiStressVec[M-1:(2*N+1)*M:M] = 0
 
-# solve for stream functions
+    psiStressVec[M-2:(2*N+1)*M:M] = 0
+    psiStressVec[M-1:(2*N+1)*M:M] = 0
 
-for i in range(2*N+1):
-    # go through each operator and z mode and generate the corresponding z mode
-    # of phi and psi
-    if i is N: continue #miss out the zeroth mode
-    phi[i*M:(i+1)*M] = linalg.solve(phiOps[i], phiStressVec[i*M:(i+1)*M])
-    psi[i*M:(i+1)*M] = linalg.solve(psiOps[i], psiStressVec[i*M:(i+1)*M])
-del i
+    # solve for stream functions
 
-### FIND VELOCITY AND PRESSURE DISTURBANCES
+    for i in range(2*N+1):
+        # go through each operator and z mode and generate the corresponding z mode
+        # of phi and psi
+        if i is N: continue #miss out the zeroth mode
+        phi[i*M:(i+1)*M] = linalg.solve(phiOps[i], phiStressVec[i*M:(i+1)*M])
+        psi[i*M:(i+1)*M] = linalg.solve(psiOps[i], psiStressVec[i*M:(i+1)*M])
+    del i
 
-#Find most of the modes 
-du = dot(1.j*kx*MDY, phi) - dot(MDZ, psi)
-dv = kx**2*phi - dot(MDZZ, phi)
-dw = dot(MDYZ, phi) + 1.j*kx*psi
+    ### FIND VELOCITY AND PRESSURE DISTURBANCES
 
-# calculate dw0 
-dw0vec = -(1.-beta)/(beta*Wi)*(-kx*1.j*dcxz[N*M:(N+1)*M] + \
-                               dot(singleMDY,dcyz[N*M:(N+1)*M]))
-                              
-# put in boundary conditions
-dw0vec[M-2] = 0 
-dw0vec[M-1] = 0
+    #Find most of the modes 
+    du = dot(1.j*kx*MDY, phi) - dot(MDZ, psi)
+    dv = kx**2*phi - dot(MDZZ, phi)
+    dw = dot(MDYZ, phi) + 1.j*kx*psi
 
-dw[N*M:(N+1)*M] = linalg.solve(dw0thOp, dw0vec)
+    # calculate dw0 
+    dw0vec = - (1.-beta)/(beta*Wi) * (kx*1.j*dcxz[N*M:(N+1)*M] + \
+                                      dot(singleMDY, dcyz[N*M:(N+1)*M]))
+                                  
+    # put in boundary conditions
+    dw0vec[M-2] = 0 
+    dw0vec[M-1] = 0
 
-# calculate dv0
-dv0vec = (1.-beta)/(beta*Wi)*( dot(singleMDY,dcxx[N*M:(N+1)*M]) \
-                              + dot( ((1./(1.j*kx))*singleMDYY - 1.j*kx), \
-                                    dcxy[N*M:(N+1)*M]) \
-                              - dot( singleMDY, dcyy[N*M:(N+1)*M] ) )
-dv0vec[M-2] = 0 
-dv0vec[M-1] = 0
+    dw[N*M:(N+1)*M] = linalg.solve(dw0thOp, dw0vec)
 
-dv[N*M:(N+1)*M] =  linalg.solve(dv0thOp, dv0vec)
+    # calculate dv0
+    dv0vec = (1.-beta)/(beta*Wi)*( dot(singleMDY,dcxx[N*M:(N+1)*M]) \
+                                  + dot( ((1./(1.j*kx))*singleMDYY - 1.j*kx), \
+                                        dcxy[N*M:(N+1)*M]) \
+                                  - dot( singleMDY, dcyy[N*M:(N+1)*M] ) )
+    dv0vec[M-2] = 0 
+    dv0vec[M-1] = 0
 
-# calculate du0
-du[N*M:(N+1)*M] = dot( (1./(1.j*kx))*singleMDY, dv[N*M:(N+1)*M] )
+    dv[N*M:(N+1)*M] =  linalg.solve(dv0thOp, dv0vec)
 
-# calculate pressure
-dp = (beta/(1.j*kx))*dot(LAPLACIAN, du) \
-    + (1.-beta)/(Wi)*( dcxx + (1./(1.j*kx))*dot(MDY,dcxy) )
+    # calculate du0
+    du[N*M:(N+1)*M] = dot( (1./(1.j*kx))*singleMDY, dv[N*M:(N+1)*M] )
 
-############TEST
-print '==================TESTS======================='
-print 'zeroth modes'
-print 'du0 has been calculated correctly: ', allclose(du_test[N*M:(N+1)*M], du[N*M:(N+1)*M])
-print 'dv0 has been calculated correctly: ', allclose(dv_test[N*M:(N+1)*M], dv[N*M:(N+1)*M])
-print 'dw0 has been calculated correctly: ', allclose(dw_test[N*M:(N+1)*M], dw[N*M:(N+1)*M])
-print 'dp0 has been calculated correctly: ', allclose(dp_test[N*M:(N+1)*M], dp[N*M:(N+1)*M])
-print '=============================================='
-print 'du has been calculated correctly: ', allclose(du_test, du)
-print 'dv has been calculated correctly: ', allclose(dv_test, dv)
-print 'dw has been calculated correctly: ', allclose(dw_test, dw)
-print 'dp has been calculated correctly: ', allclose(dp_test, dp)
+    # calculate pressure
+    dp = (beta/(1.j*kx))*dot(LAPLACIAN, du) \
+        + (1.-beta)/(Wi)*( dcxx + (1./(1.j*kx))*dot(MDY,dcxy) )
 
+
+    # ---TESTS-----------------------------------------------------------------------
+    #print '==================TESTS======================='
+    #print 'zeroth modes'
+    #print 'du0 has been calculated correctly: ', allclose(du_test[N*M:(N+1)*M], du[N*M:(N+1)*M])
+    #print 'dv0 has been calculated correctly: ', allclose(dv_test[N*M:(N+1)*M], dv[N*M:(N+1)*M])
+    #print 'dw0 has been calculated correctly: ', allclose(dw_test[N*M:(N+1)*M], dw[N*M:(N+1)*M])
+    #print 'dp0 has been calculated correctly: ', allclose(dp_test[N*M:(N+1)*M], dp[N*M:(N+1)*M])
+    #print '=============================================='
+    #print 'du has been calculated correctly: ', allclose(du_test, du)
+    #print 'dv has been calculated correctly: ', allclose(dv_test, dv)
+    #print 'dw has been calculated correctly: ', allclose(dw_test, dw)
+    #print 'dp has been calculated correctly: ', allclose(dp_test, dp)
+
+    #duData = vstack((du_test[N*M:(N+1)*M], du[N*M:(N+1)*M])).T
+    #dvData = vstack((dv_test[N*M:(N+1)*M], dv[N*M:(N+1)*M])).T
+    #dwData = vstack((dw_test[N*M:(N+1)*M], dw[N*M:(N+1)*M])).T
+
+    #savetxt("du_comp.dat", (duData))
+    #savetxt("dv_comp.dat", (dvData))
+    #savetxt("dw_comp.dat", (dwData))
+
+    #print "exit before time step."
+    #exit(1)
+    # -------------------------------------------------------------------------------
+
+    ### TAKE A TIME STEP IN THE CONFORMATION VECTORS
+
+    # make a vector containing the results of the RHS of the iteration equations.
+
+    iterVec = zeros(6*vecLen, dtype='complex')
+
+    #  cxx
+    iterVec[:vecLen] = - oneOverWi*dcxx - dot(VGRAD, dcxx) \
+                     - dot(MMDYCXX, dv) - dot(MMDZCXX, dw) + 2.j*kx*dot(MMCXX, du)\
+                     + 2*dot(dot(MMCXY, MDY), du) + 2*dot( dot(MMCXZ, MDZ), du) \
+                     + 2*dot(MMDYU, dcxy) + 2*dot(MMDZU, dcxz)
+    # cyy
+    iterVec[1*vecLen:2*vecLen] = - oneOverWi*dcyy - dot(VGRAD, dcyy) \
+                     - dot(MMDYCYY, dv) - dot(MMDZCYY, dw) + 2.j*kx*dot(MMCXY, dv)\
+                     + 2*dot(dot(MMCYY, MDY), dv)  + 2*dot(dot(MMCYZ, MDZ), dv) \
+                     + 2*dot(MMDYV, dcyy) + 2*dot(MMDZV, dcyz)
+
+                            
+    # czz
+    iterVec[2*vecLen:3*vecLen] = - oneOverWi*dczz - dot(VGRAD, dczz) \
+                     - dot(MMDYCZZ, dv) - dot(MMDZCZZ, dw) + 2.j*kx*dot(MMCXZ, dv)\
+                     + 2*dot(dot(MMCYZ, MDY), dw) + 2*dot(dot(MMCZZ, MDZ), dw) \
+                     + 2*dot(MMDYW, dcyz) + 2*dot(MMDZW, dczz)
+
+    # cxy
+    iterVec[3*vecLen:4*vecLen] = - oneOverWi*dcxy - dot(VGRAD, dcxy) \
+                     - dot(MMDYCXY, dv) - dot(MMDZCXY, dw) \
+                     + dot(dot(MMCYY, MDY) + dot(MMCYZ, MDZ), du) \
+                     + dot(MMDYU, dcyy) + dot(MMDZU, dcyz) + 1.j*kx*dot(MMCXX, dv)\
+                     + dot(dot(MMCXZ, MDZ), dv) + dot(MMDYV, dcxy) \
+                     + dot(MMDZV, dcxz) - dot(dot(MMCXY, MDZ), dw)
+
+    # cxz
+    iterVec[4*vecLen:5*vecLen] = - oneOverWi*dcxz - dot(VGRAD, dcxz) \
+                     - dot(MMDYCXZ, dv) - dot(MMDZCXZ, dw) \
+                     + dot(dot(MMCYZ, MDY), du) + dot(dot(MMCZZ, MDZ), du) \
+                     + dot(MMDYU, dcyz) + dot(MMDZU, dczz) + 1.j*dot(MMCXX, dw) \
+                     + dot(dot(MMCXY, MDY), dw) + dot(MMDYW, dcxy) \
+                     + dot(MMDZW, dcxz) - dot(dot(MMCXZ, MDY), dv)
+
+    # cyz
+    iterVec[4*vecLen:5*vecLen] = - oneOverWi*dcyz - dot(VGRAD, dcyz) \
+                     - dot(MMDYCYZ, dv) - dot(MMDZCYZ, dw) \
+                     + 1.j*dot(MMCXZ, dv) + dot(dot(MMCZZ, MDZ), dv) \
+                     + dot(MMDZV, dczz) + 1.j*kx*dot(MMCXY, dw) \
+                     + dot(dot(MMCYY, MDY), dw) + dot(MMDYW, dcyy) \
+                     - 1.j*kx*dot(MMCYZ, du)
+
+    # do an iteration of the conformations (Euler for now)
+    cnew = cvec + TIMESTEP*iterVec
+    cvec = cnew
+
+    # save the norm of the disturbance strength and the time to a file
+    strength = linalg.norm(concatenate((du,dv,dw,cvec)),2)
+    outFile.write('{0:10.5g} {1:10.5g}'.format(currTime, strength))
+    outFile.flush()
+del curr_time
+
+outFile.close()
+
+# Tidy up and dump the final disturbance vector to a file
+dcxx = cvec[:1*vecLen]
+dcyy = cvec[vecLen:2*vecLen]
+dczz = cvec[2*vecLen:3*vecLen]
+dcxy = cvec[3*vecLen:4*vecLen]
+dcxz = cvec[4*vecLen:5*vecLen]
+dcyz = cvec[5*vecLen:6*vecLen]
+
+output = (du, dv, dw, dp, dcxx, dcyy, dczz, dcxy, dcxz, dcyz)
+pickle.dump(output, open('final-dist-vec{0}'.format(filename), 'w'))
+
+print "DONE!"
